53db4045f63f35ab6e8399fe483c35f10e5a7992
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-25 08:50:05 +0530

    ADDED:Method for unparking the car

--- src/main/java/com/bl/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bl/parkinglotsystem/ParkingLotSystem.java
@@ -1,7 +1,17 @@
 package com.bl.parkinglotsystem;
 
 public class ParkingLotSystem {
+    private Object vehicle;
+
     public boolean park(Object vehicle) {
+        this.vehicle = vehicle;
         return true;
+    }
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
