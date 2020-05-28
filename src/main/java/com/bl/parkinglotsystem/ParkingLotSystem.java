a2af79888eabea1ec33a13f462f89ffce77a1cfa
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-28 19:21:34 +0530

    MODIFIED:Adding getSlot method

--- src/main/java/com/bl/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bl/parkinglotsystem/ParkingLotSystem.java
@@ -7,27 +7,22 @@ import java.util.List;
 public class ParkingLotSystem {
 
     private final List parkedVehicles;
-    private SlotAllotment slotManager = null;
+    private SlotAllotment slotManager;
     private int parkingCapacity;
     private boolean parkingCapacityFull;
-
-
     public ParkingLotSystem(int parkingCapacity) {
         this.parkedVehicles = new ArrayList(parkingCapacity);
         this.slotManager = new SlotAllotment(parkingCapacity);
     }
-
     public void setParkingLotCapacity(int capacity) {
         this.parkingCapacity = capacity;
     }
-
     public boolean isThisCarPresentInTheParkingLot(Object vehicle) {
         if (this.parkedVehicles.contains(vehicle)) {
             return true;
         }
         return false;
     }
-
     public void parkTheCar(Object vehicle) throws ParkingLotSystemException {
         if (this.parkedVehicles.size() == this.parkingCapacity) {
             this.parkingCapacityFull = true;
@@ -38,6 +33,13 @@ public class ParkingLotSystem {
             throw new ParkingLotSystemException("Car already present in parking lot!",
                     ParkingLotSystemException.ExceptionType.CAR_ALREADY_PARKED);
         }
+        int slot = this.getSlot();
+        this.parkedVehicles.add(slot,vehicle);
+        this.slotManager.parkUpdate(vehicle,slot);
+    }
+
+    private int getSlot() throws ParkingLotSystemException {
+        return slotManager.getNearestParkingSlot()-1;
     }
 
     public void unParkTheCar(Object vehicle) throws ParkingLotSystemException {
