38093a53b2435408a148e50edec53a568ce04c25
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-29 14:49:55 +0530

    ADDED:Police department class for investigation

--- src/main/java/com/bl/parkinglotsystem/Vehicle.java
+++ src/main/java/com/bl/parkinglotsystem/Vehicle.java
@@ -4,15 +4,20 @@ public class Vehicle {
     DriverType driverType;
     private String vehicleId;
     VehicleSize vehicleSize;
+    VehicleColour vehicleColour;
 
-    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize) {
+    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize, VehicleColour vehicleColour) {
         this.vehicleId = vehicleId;
         this.driverType = driverType;
         this.vehicleSize = vehicleSize;
+        this.vehicleColour =  vehicleColour;
     }
     public String getVehicleId() {
         return vehicleId;
     }
     public enum DriverType {HANDICAP, NORMAL}
     public enum VehicleSize {LARGE, SMALL}
+    public enum VehicleColour {
+        WHITE
+    }
 }
