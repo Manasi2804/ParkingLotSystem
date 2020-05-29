36d7bcad27252457837ae466c92bd1cce4468d30
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-29 08:58:58 +0530

    ADDED:Vehicle size enum class

--- src/main/java/com/bl/parkinglotsystem/Vehicle.java
+++ src/main/java/com/bl/parkinglotsystem/Vehicle.java
@@ -3,13 +3,16 @@ package com.bl.parkinglotsystem;
 public class Vehicle {
     DriverType driverType;
     private String vehicleId;
+    VehicleSize vehicleSize;
 
-    public Vehicle(String vehicleId, DriverType driverType) {
+    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize) {
         this.vehicleId = vehicleId;
         this.driverType = driverType;
+        this.vehicleSize = vehicleSize;
     }
     public String getVehicleId() {
         return vehicleId;
     }
     public enum DriverType {HANDICAP, NORMAL}
+    public enum VehicleSize {LARGE, SMALL}
 }
